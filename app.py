c178575671f9045b59224f9876193a3fb2ac9988
Alejo <aleedin14@gmail.com>
2026-05-26 21:27:09 -0300

    creando funciones.py y creando la fuincion partidos ganados()

--- app.py
+++ app.py
@@ -11,22 +11,9 @@
 # ● Generar un gráfico comparativo de rendimiento entre equipos.
 
 #importo el archivo con los resultados de los partidos
-import pandas as pd
-df = pd.read_csv('resultados_torneo.csv',sep=';',encoding='latin-1')
-# print(df)
-#calculo la cantidad de partidos ganados por cada equipo
-victorias= {}
-for _, partido in df.iterrows():
-    equipo_local = partido['Equipo_Local']
-    equipo_visitante = partido['Equipo_Visitante']
-    goles_local = partido['Goles_Local']
-    goles_visitante = partido['Goles_Visitante']
-    if equipo_local not in victorias:
-        victorias[equipo_local ] = 0
-    if equipo_visitante not in victorias:
-        victorias[equipo_visitante] = 0
-    if goles_local > goles_visitante:
-        victorias[equipo_local] += 1
-    elif goles_visitante > goles_local:
-        victorias[equipo_visitante] += 1
-print(victorias)
+from funciones import partidos_ganados
+
+
+if __name__ == "__main__":
+    partidos_ganados()
+    
